035ab4322b12d64f3e26495dfd97f96da83c1016
LightRepository <artemburakov.net@mail.ru>
2026-04-09 08:53:27 +0400

    webmaster

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="yandex-verification" content="65f0ed8b637c35ad" />
     <title>СеверСтрой - Инертные материалы с доставкой</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
